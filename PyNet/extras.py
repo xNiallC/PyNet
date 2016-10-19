f9bbad5d603469c2ac3497917986424d4b4fd12e
Clamii <callumjhughes@gmail.com>
2016-10-19 20:21:03 +0100

    minor grammar change - intro

--- PyNet/extras.py
+++ PyNet/extras.py
@@ -1,9 +1,9 @@
 intro = """------ PyNet: A Game by Team 10 ------
-'One man. One desire. The goal to create a device protect himself and those around him, the only way he knew how.
+'One man. One desire. The goal to create a device to protect himself and those around him, the only way he knew how.
 Python.' The words echo in your (Kirill Sodorov, Expert Programmer, Overall Nice Guy) head. Years of planning,
 programming, bug fixing, getting angry, and progressive montages led to this moment. Over a cup of lukewarm coffee,
 you finish the last function:- 'def killcode'. You lean back and revel in your own glory, ready to get a good
-night's sleep to prepare for the big presentation in the University tomorrow. Researchers from round the country are
+night's sleep to prepare for the big presentation in the University tomorrow. Researchers from around the country are
 coming to witness your brainchild. This could be your big break. Let's hope everything goes to plan. You didn't
 forget to logoff the lecture hall computer did you?
 """
