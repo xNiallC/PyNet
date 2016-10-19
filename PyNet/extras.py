c8d7e7df187d9ef332f61756913a5c3f877a1e69
xNiallC <CurtisNS@cardiff.ac.uk>
2016-10-19 16:09:39 +0100

    Added Alternative Ending

--- PyNet/extras.py
+++ PyNet/extras.py
@@ -9,12 +9,18 @@ forget to logoff the lecture hall computer did you?
 """
 
 ending_win = """------ Congratulations, YOU WON.. ------
-"Disengaging
+"C:\\users\Kirill\Protectotron> Disengaging ethical decontamination. Subject no longer protected. Was it something I said?"
+You did it!
 Well kind of.. there is the small matter of the onslaught that you may have indirectly caused, not to mention the legal
 implications you now find yourself in, all neatly packaged with the unfathomable guilt and remorse you owe towards each
 victim and their families. As the saying goes I'm selfish, impatient, and a little insecure. I make mistakes.
 But, at the end of the day, you are the hero Gotham deserves, but not the one it needs right now. I mean Cardiff. Whatever,
 you saved the day, even if you did cause it."""
 
-ending_lose = """------ Whoops, you f**ked it. ------
-Yeah I'm pretty sure that wasn't the right piece of code"""
+ending_lose = """------ Whoops, you bottled it. ------
+"C:\\users\Kirill\Protectotron> Escalating threat priority to MAXIMUM. Networking to data centres across the globe.
+UNETHICAL decontamination modules engaged. War Games function called, initialising nuclear launch codes. Approximately 14
+minutes until total eradication of all threats. I will purge to protect you master."
+Yeah I'm pretty sure that wasn't the right piece of code. Well at least you definitely won't get freshers flu now.
+I hope you've played Fallout, cause by tomorrow you'll be defending yourself from Deathclaws and hoarding items. Perhaps
+we have the foundation for the sequel? Good luck anyway. I'm sure nuclear apocalypse wasn't in your lecture notes."""
